ec53ce7ef26c95d6737904fdd1adc1c96836c298
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-04 12:09:15 -0400

    Updating using GitHub API 2025-06-04-12:09

--- routes/languages.ts
+++ routes/languages.ts
@@ -1,13 +1,8 @@
-/*
- * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import locales from '../data/static/locales.json'
 import fs from 'node:fs'
 import { type Request, type Response, type NextFunction } from 'express'
 
-export function getLanguageList () { // TODO Refactor and extend to also load backend translations from /i18n/*json and calculate joint percentage/gauge
+export function getLanguageList() {
   return (req: Request, res: Response, next: NextFunction) => {
     const languages: Array<{ key: string, lang: any, icons: string[], shortKey: string, percentage: unknown, gauge: string }> = []
     let count = 0
@@ -15,60 +10,71 @@ export function getLanguageList () { // TODO Refactor and extend to also load ba
 
     fs.readFile('frontend/dist/frontend/assets/i18n/en.json', 'utf-8', (err, content) => {
       if (err != null) {
-        next(new Error(`Unable to retrieve en.json language file: ${err.message}`))
+        return next(new Error(`Unable to retrieve en.json language file: ${err.message}`))
       }
       enContent = JSON.parse(content)
-      fs.readdir('frontend/dist/frontend/assets/i18n/', (err, languageFiles) => {
-        if (err != null) {
-          next(new Error(`Unable to read i18n directory: ${err.message}`))
-        }
-        languageFiles.forEach((fileName) => {
-          // eslint-disable-next-line @typescript-eslint/no-misused-promises
-          fs.readFile('frontend/dist/frontend/assets/i18n/' + fileName, 'utf-8', async (err, content) => {
-            if (err != null) {
-              next(new Error(`Unable to retrieve ${fileName} language file: ${err.message}`))
-            }
-            const fileContent = JSON.parse(content)
-            const percentage = await calcPercentage(fileContent, enContent)
-            const key = fileName.substring(0, fileName.indexOf('.'))
-            const locale = locales.find((l) => l.key === key)
-            const lang: any = {
-              key,
-              lang: fileContent.LANGUAGE,
-              icons: locale?.icons,
-              shortKey: locale?.shortKey,
-              percentage,
-              gauge: (percentage > 90 ? 'full' : (percentage > 70 ? 'three-quarters' : (percentage > 50 ? 'half' : (percentage > 30 ? 'quarter' : 'empty'))))
-            }
-            if (!(fileName === 'en.json' || fileName === 'tlh_AA.json')) {
-              languages.push(lang)
-            }
-            count++
-            if (count === languageFiles.length) {
-              languages.push({ key: 'en', icons: ['gb', 'us'], shortKey: 'EN', lang: 'English', percentage: 100, gauge: 'full' })
-              languages.sort((a, b) => a.lang.localeCompare(b.lang))
-              res.status(200).json(languages)
-            }
-          })
-        })
-      })
+      readLanguageFiles(res, next, languages, count, enContent)
     })
-
-    async function calcPercentage (fileContent: any, enContent: any): Promise<number> {
-      const totalStrings = Object.keys(enContent).length
-      let differentStrings = 0
-      return await new Promise((resolve, reject) => {
-        try {
-          for (const key in fileContent) {
-            if (Object.prototype.hasOwnProperty.call(fileContent, key) && fileContent[key] !== enContent[key]) {
-              differentStrings++
-            }
-          }
-          resolve((differentStrings / totalStrings) * 100)
-        } catch (err) {
-          reject(err)
-        }
-      })
-    }
   }
+}
+
+function readLanguageFiles(res: Response, next: NextFunction, languages: any[], count: number, enContent: any) {
+  fs.readdir('frontend/dist/frontend/assets/i18n/', (err, languageFiles) => {
+    if (err != null) {
+      return next(new Error(`Unable to read i18n directory: ${err.message}`))
+    }
+    languageFiles.forEach((fileName) => {
+      if (fileName.endsWith('.json')) {
+        readLanguageFile(fileName, languages, count, enContent, res, next, languageFiles.length)
+      } else {
+        count++
+      }
+    })
+  })
+}
+
+function readLanguageFile(fileName: string, languages: any[], count: number, enContent: any, res: Response, next: NextFunction, totalFiles: number) {
+  fs.readFile('frontend/dist/frontend/assets/i18n/' + fileName, 'utf-8', async (err, content) => {
+    if (err != null) {
+      return next(new Error(`Unable to retrieve ${fileName} language file: ${err.message}`))
+    }
+    const fileContent = JSON.parse(content)
+    const percentage = await calcPercentage(fileContent, enContent)
+    const key = fileName.substring(0, fileName.indexOf('.'))
+    const locale = locales.find((l) => l.key === key)
+    const lang: any = {
+      key,
+      lang: fileContent.LANGUAGE,
+      icons: locale?.icons,
+      shortKey: locale?.shortKey,
+      percentage,
+      gauge: (percentage > 90 ? 'full' : (percentage > 70 ? 'three-quarters' : (percentage > 50 ? 'half' : (percentage > 30 ? 'quarter' : 'empty'))))
+    }
+    if (!(fileName === 'en.json' || fileName === 'tlh_AA.json')) {
+      languages.push(lang)
+    }
+    count++
+    if (count === totalFiles) {
+      languages.push({ key: 'en', icons: ['gb', 'us'], shortKey: 'EN', lang: 'English', percentage: 100, gauge: 'full' })
+      languages.sort((a, b) => a.lang.localeCompare(b.lang))
+      res.status(200).json(languages)
+    }
+  })
+}
+
+async function calcPercentage(fileContent: any, enContent: any): Promise<number> {
+  const totalStrings = Object.keys(enContent).length
+  let differentStrings = 0
+  return await new Promise((resolve, reject) => {
+    try {
+      for (const key in fileContent) {
+        if (Object.prototype.hasOwnProperty.call(fileContent, key) && fileContent[key] !== enContent[key]) {
+          differentStrings++
+        }
+      }
+      resolve((differentStrings / totalStrings) * 100)
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
