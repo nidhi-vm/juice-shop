da1180a4bec3e8be9c11051ae409564a55818adf
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-11 11:44:51 -0400

    Updating using GitHub API 2025-06-11-11:44

--- test/cypress/e2e/geoStalking.spec.ts
+++ test/cypress/e2e/geoStalking.spec.ts
@@ -4,41 +4,29 @@ describe('/#/photo-wall', () => {
     cy.intercept('GET', '/rest/user/security-question?email=*').as('securityQuestion')
   })
 
+  const handleSecurityQuestion = (challenge: string, email: string, answerKey: string) => {
+    cy.task<string>('GetFromMemories', answerKey).then((answer: string) => {
+      cy.task<string>('GetFromConfig', 'application.domain').then((appDomain: string) => {
+        cy.get('#email').type(`${email}@${appDomain}`)
+        cy.wait('@securityQuestion')
+        cy.get('#securityAnswer').should('not.be.disabled').focus().type(answer)
+        cy.get('#newPassword').focus().type('123456')
+        cy.get('#newPasswordRepeat').focus().type('123456')
+        cy.get('#resetButton').click()
+        cy.expectChallengeSolved({ challenge })
+      })
+    })
+  }
+
   describe('challenge "geoStalkingMeta"', () => {
     it('Should be possible to find the answer to a security question in the meta-data of a photo on the photo wall', () => {
-      cy.task<string>('GetFromMemories', 'geoStalkingMetaSecurityAnswer').then(
-        (answer: string) => {
-          cy.task<string>('GetFromConfig', 'application.domain').then((appDomain: string) => {
-            cy.get('#email').type(`john@${appDomain}`)
-            cy.wait('@securityQuestion')
-            cy.get('#securityAnswer').should('not.be.disabled').focus().type(answer)
-            cy.get('#newPassword').focus().type('123456')
-            cy.get('#newPasswordRepeat').focus().type('123456')
-            cy.get('#resetButton').click()
-
-            cy.expectChallengeSolved({ challenge: 'Meta Geo Stalking' })
-          })
-        }
-      )
+      handleSecurityQuestion('Meta Geo Stalking', 'john', 'geoStalkingMetaSecurityAnswer')
     })
   })
 
   describe('challenge "geoStalkingVisual"', () => {
     it('Should be possible to determine the answer to a security question by looking closely at an image on the photo wall', () => {
-      cy.task<string>('GetFromMemories', 'geoStalkingVisualSecurityAnswer').then(
-        (answer: string) => {
-          cy.task<string>('GetFromConfig', 'application.domain').then((appDomain: string) => {
-            cy.get('#email').type(`emma@${appDomain}`)
-            cy.wait('@securityQuestion')
-            cy.get('#securityAnswer').should('not.be.disabled').focus().type(answer)
-            cy.get('#newPassword').focus().type('123456')
-            cy.get('#newPasswordRepeat').focus().type('123456')
-            cy.get('#resetButton').click()
-
-            cy.expectChallengeSolved({ challenge: 'Visual Geo Stalking' })
-          })
-        }
-      )
+      handleSecurityQuestion('Visual Geo Stalking', 'emma', 'geoStalkingVisualSecurityAnswer')
     })
   })
 })
