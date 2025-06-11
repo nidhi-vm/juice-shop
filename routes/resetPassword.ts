03c18b485041b21efefe76f7138b4e0bc6afafbe
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-11 11:44:45 -0400

    Updating using GitHub API 2025-06-11-11:44

--- routes/resetPassword.ts
+++ routes/resetPassword.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import config from 'config'
 import { type Request, type Response, type NextFunction } from 'express'
 
@@ -13,73 +8,70 @@ import { challenges, users } from '../data/datacache'
 import * as security from '../lib/insecurity'
 import { UserModel } from '../models/user'
 
-export function resetPassword () {
-  return ({ body, connection }: Request, res: Response, next: NextFunction) => {
+export function resetPassword() {
+  return async ({ body, connection }: Request, res: Response, next: NextFunction) => {
     const email = body.email
     const answer = body.answer
     const newPassword = body.new
     const repeatPassword = body.repeat
+
     if (!email || !answer) {
-      next(new Error('Blocked illegal activity by ' + connection.remoteAddress))
-    } else if (!newPassword || newPassword === 'undefined') {
-      res.status(401).send(res.__('Password cannot be empty.'))
-    } else if (newPassword !== repeatPassword) {
-      res.status(401).send(res.__('New and repeated password do not match.'))
-    } else {
-      SecurityAnswerModel.findOne({
+      return next(new Error('Blocked illegal activity by ' + connection.remoteAddress))
+    }
+    if (!newPassword || newPassword === 'undefined') {
+      return res.status(401).send(res.__('Password cannot be empty.'))
+    }
+    if (newPassword !== repeatPassword) {
+      return res.status(401).send(res.__('New and repeated password do not match.'))
+    }
+
+    try {
+      const data = await SecurityAnswerModel.findOne({
         include: [{
           model: UserModel,
           where: { email }
         }]
-      }).then((data: SecurityAnswerModel | null) => {
-        if ((data != null) && security.hmac(answer) === data.answer) {
-          UserModel.findByPk(data.UserId).then((user: UserModel | null) => {
-            user?.update({ password: newPassword }).then((user: UserModel) => {
-              verifySecurityAnswerChallenges(user, answer)
-              res.json({ user })
-            }).catch((error: unknown) => {
-              next(error)
-            })
-          }).catch((error: unknown) => {
-            next(error)
-          })
-        } else {
-          res.status(401).send(res.__('Wrong answer to security question.'))
-        }
-      }).catch((error: unknown) => {
-        next(error)
       })
+
+      if (data != null && security.hmac(answer) === data.answer) {
+        const user = await UserModel.findByPk(data.UserId)
+        await user?.update({ password: newPassword })
+        verifySecurityAnswerChallenges(user, answer)
+        return res.json({ user })
+      } else {
+        return res.status(401).send(res.__('Wrong answer to security question.'))
+      }
+    } catch (error) {
+      return next(error)
     }
   }
 }
 
-function verifySecurityAnswerChallenges (user: UserModel, answer: string) {
-  challengeUtils.solveIf(challenges.resetPasswordJimChallenge, () => { return user.id === users.jim.id && answer === 'Samuel' })
-  challengeUtils.solveIf(challenges.resetPasswordBenderChallenge, () => { return user.id === users.bender.id && answer === 'Stop\'n\'Drop' })
-  challengeUtils.solveIf(challenges.resetPasswordBjoernChallenge, () => { return user.id === users.bjoern.id && answer === 'West-2082' })
-  challengeUtils.solveIf(challenges.resetPasswordMortyChallenge, () => { return user.id === users.morty.id && answer === '5N0wb41L' })
-  challengeUtils.solveIf(challenges.resetPasswordBjoernOwaspChallenge, () => { return user.id === users.bjoernOwasp.id && answer === 'Zaya' })
-  challengeUtils.solveIf(challenges.resetPasswordUvoginChallenge, () => { return user.id === users.uvogin.id && answer === 'Silence of the Lambs' })
+function verifySecurityAnswerChallenges(user: UserModel, answer: string) {
+  challengeUtils.solveIf(challenges.resetPasswordJimChallenge, () => user.id === users.jim.id && answer === 'Samuel')
+  challengeUtils.solveIf(challenges.resetPasswordBenderChallenge, () => user.id === users.bender.id && answer === 'Stop\'n\'Drop')
+  challengeUtils.solveIf(challenges.resetPasswordBjoernChallenge, () => user.id === users.bjoern.id && answer === 'West-2082')
+  challengeUtils.solveIf(challenges.resetPasswordMortyChallenge, () => user.id === users.morty.id && answer === '5N0wb41L')
+  challengeUtils.solveIf(challenges.resetPasswordBjoernOwaspChallenge, () => user.id === users.bjoernOwasp.id && answer === 'Zaya')
+  challengeUtils.solveIf(challenges.resetPasswordUvoginChallenge, () => user.id === users.uvogin.id && answer === 'Silence of the Lambs')
+  
   challengeUtils.solveIf(challenges.geoStalkingMetaChallenge, () => {
-    const securityAnswer = ((() => {
-      const memories = config.get<MemoryConfig[]>('memories')
-      for (let i = 0; i < memories.length; i++) {
-        if (memories[i].geoStalkingMetaSecurityAnswer) {
-          return memories[i].geoStalkingMetaSecurityAnswer
-        }
-      }
-    })())
+    const securityAnswer = getSecurityAnswer('geoStalkingMetaSecurityAnswer')
     return user.id === users.john.id && answer === securityAnswer
   })
+
   challengeUtils.solveIf(challenges.geoStalkingVisualChallenge, () => {
-    const securityAnswer = ((() => {
-      const memories = config.get<MemoryConfig[]>('memories')
-      for (let i = 0; i < memories.length; i++) {
-        if (memories[i].geoStalkingVisualSecurityAnswer) {
-          return memories[i].geoStalkingVisualSecurityAnswer
-        }
-      }
-    })())
+    const securityAnswer = getSecurityAnswer('geoStalkingVisualSecurityAnswer')
     return user.id === users.emma.id && answer === securityAnswer
   })
+}
+
+function getSecurityAnswer(answerKey: string): string | undefined {
+  const memories = config.get<MemoryConfig[]>('memories')
+  for (const memory of memories) {
+    if (memory[answerKey]) {
+      return memory[answerKey]
+    }
+  }
+  return undefined
 }
