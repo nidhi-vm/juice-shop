cf5b8928fd1e7cd897ca6d1a3cef6f23c5d6f43e
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-11 11:44:57 -0400

    Updating using GitHub API 2025-06-11-11:44

--- routes/deluxe.ts
+++ routes/deluxe.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import { type Request, type Response, type NextFunction } from 'express'
 
 import * as challengeUtils from '../lib/challengeUtils'
@@ -24,7 +19,7 @@ export function upgradeToDeluxe () {
       if (req.body.paymentMode === 'wallet') {
         const wallet = await WalletModel.findOne({ where: { UserId: req.body.UserId } })
         if ((wallet != null) && wallet.balance < 49) {
-          res.status(400).json({ status: 'error', error: 'Insuffienct funds in Wallet' })
+          res.status(400).json({ status: 'error', error: 'Insufficient funds in Wallet' })
           return
         } else {
           await WalletModel.decrement({ balance: 49 }, { where: { UserId: req.body.UserId } })
@@ -49,10 +44,10 @@ export function upgradeToDeluxe () {
         security.authenticatedUsers.put(updatedToken, userWithStatus)
         res.status(200).json({ status: 'success', data: { confirmation: 'Congratulations! You are now a deluxe member!', token: updatedToken } })
       } catch (error) {
-        res.status(400).json({ status: 'error', error: 'Something went wrong. Please try again!' })
+        next(error) // Pass the error to the next middleware
       }
     } catch (err: unknown) {
-      res.status(400).json({ status: 'error', error: 'Something went wrong: ' + utils.getErrorMessage(err) })
+      next(err) // Pass the error to the next middleware
     }
   }
 }
