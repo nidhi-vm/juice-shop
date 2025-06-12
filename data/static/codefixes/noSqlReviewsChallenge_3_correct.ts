c23a58fa41ca562e99a322994731373d456c3131
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-12 14:55:46 -0400

    Updating using GitHub API 2025-06-12-14:55

--- data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
+++ data/static/codefixes/noSqlReviewsChallenge_3_correct.ts
@@ -1,6 +1,6 @@
 export function updateProductReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
-    const user = security.authenticatedUsers.from(req)
+    security.authenticatedUsers.from(req)
 
     if (typeof req.body.id !== 'string') {
       res.status(400).send()
