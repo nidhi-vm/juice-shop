53dd41ec0ca49c305f91a2c44ad8f1faefa2e2c7
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-15 21:28:41 -0400

    Updating using GitHub API 2025-06-15-21:28

--- lib/noUpdate.ts
+++ lib/noUpdate.ts
@@ -1,7 +1,6 @@
 // Credit for the implementation in JS: https://github.com/daviddossantos/sequelize-notupdate-attributes
 import { type Model, type ValidationErrorItemType } from 'sequelize/types'
 import { type ValidationOptions } from 'sequelize/types/instance-validator'
-// @ts-expect-error FIXME due to non-existing type definitions for sequelize/lib/errors
 import { ValidationError, ValidationErrorItem } from 'sequelize/lib/errors'
 
 interface ExtendedValidationOptions extends ValidationOptions {
