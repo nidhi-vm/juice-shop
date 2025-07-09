bd243c332de834e15dc4627e7f9dd9b7f75e7546
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:48:59 -0400

    Updating using GitHub API 2025-07-09-14:48

--- frontend/src/app/web3-sandbox/web3-sandbox.component.ts
+++ frontend/src/app/web3-sandbox/web3-sandbox.component.ts
@@ -309,8 +309,8 @@ contract HelloWorld {
       }
       console.log('session', this.session)
       this.changeDetectorRef.detectChanges()
-    } catch (error) {
-      console.error('An error occurred during authentication:', error)
+    } catch (err) {
+      console.log('An error occurred')
     }
   }
 }
