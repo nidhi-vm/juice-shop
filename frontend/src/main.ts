0c75d84bb34621338c0c5c629784385fabd8aa20
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:54:51 -0400

    Updating using GitHub API 2025-07-09-14:54

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,7 @@
  */
 
 import { enableProdMode, importProvidersFrom } from '@angular/core'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
-
+import { AppModule, HttpLoaderFactory } from './app/app.module'
 import { environment } from './environments/environment'
 import { AppComponent } from './app/app.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
@@ -47,6 +46,7 @@ import { GalleryModule } from 'ng-gallery';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { Routing } from './app/app.routing';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
@@ -91,7 +91,7 @@ bootstrapApplication(AppComponent, {
         importProvidersFrom(BrowserModule, Routing, TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
-                useFactory: () => new TranslateHttpLoader(HttpClient),
+                useFactory: HttpLoaderFactory,
                 deps: [HttpClient]
             }
         }), CookieModule.forRoot(), FlexLayoutModule, ReactiveFormsModule, GalleryModule, NgxTextDiffModule, QrCodeModule, FileUploadModule, ClipboardModule, MatToolbarModule, MatIconModule, FormsModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatSidenavModule, MatRippleModule, MatTableModule, MatPaginatorModule, MatCardModule, MatInputModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatDatepickerModule, MatNativeDateModule, MatExpansionModule, MatProgressBarModule, MatTooltipModule, MatMenuModule, MatListModule, MatButtonToggleModule, LayoutModule, MatGridListModule, MatBadgeModule, MatRadioModule, MatSnackBarModule, MatSliderModule, MatTabsModule, MatSlideToggleModule, MatChipsModule, MatAutocompleteModule, HighlightModule),
