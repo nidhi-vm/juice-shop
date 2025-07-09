aa4586913a1755829a42f630d77a14a226b03b2d
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:42:50 -0400

    Updating using GitHub API 2025-07-09-14:42

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2014-2025 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import fs from 'node:fs'
 import { Readable } from 'node:stream'
 import { finished } from 'node:stream/promises'
