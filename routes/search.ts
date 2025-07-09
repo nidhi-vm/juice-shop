277083dc0b3dd68c6bf296db9f4677c8d4392f20
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:49:04 -0400

    Updating using GitHub API 2025-07-09-14:49

--- routes/search.ts
+++ routes/search.ts
@@ -44,7 +44,7 @@ export function searchProducts () {
         }
         if (challengeUtils.notSolved(challenges.dbSchemaChallenge)) {
           let solved = true
-          models.sequelize.query('SELECT sql FROM sqlite_master').then(([data]: any) => {
+          void models.sequelize.query('SELECT sql FROM sqlite_master').then(([data]: any) => {
             const tableDefinitions = utils.queryResultToJson(data)
             if (tableDefinitions.data?.length) {
               for (let i = 0; i < tableDefinitions.data.length; i++) {
