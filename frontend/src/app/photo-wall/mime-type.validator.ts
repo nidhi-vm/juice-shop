2434c6c764494a152d855317bcad1587986a176a
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:53:03 -0400

    Updating using GitHub API 2025-07-09-14:53

--- frontend/src/app/photo-wall/mime-type.validator.ts
+++ frontend/src/app/photo-wall/mime-type.validator.ts
@@ -20,13 +20,11 @@ export const mimeType = (
         const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4)
         let header = ''
         let isValid = false
-        for (let i = 0; i < arr.length; i++) {
-          header += arr[i].toString(16)
+        for (const byte of arr) {
+          header += byte.toString(16)
         }
         switch (header) {
           case '89504e47':
-            isValid = true
-            break
           case 'ffd8ffe0':
           case 'ffd8ffe1':
           case 'ffd8ffe2':
