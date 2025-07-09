9d8de4b94166d6a08b812b6989e622783102a0fb
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:42:44 -0400

    Updating using GitHub API 2025-07-09-14:42

--- routes/likeProductReviews.ts
+++ routes/likeProductReviews.ts
@@ -53,7 +53,7 @@ export function likeProductReviews () {
         )
         res.json(result)
       } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json({ error: 'Internal Server Error' })
       }
     } catch (err) {
       res.status(400).json({ error: 'Wrong Params' })
