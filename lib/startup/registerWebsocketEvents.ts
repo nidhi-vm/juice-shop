658e01e65029e20dff5bb7ea5457ccca66cbe4d0
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-07-09 14:45:56 -0400

    Updating using GitHub API 2025-07-09-14:45

--- lib/startup/registerWebsocketEvents.ts
+++ lib/startup/registerWebsocketEvents.ts
@@ -18,7 +18,6 @@ const globalWithSocketIO = global as typeof globalThis & {
 
 const registerWebsocketEvents = (server: any) => {
   const io = new Server(server, { cors: { origin: 'http://localhost:4200' } })
-  // @ts-expect-error FIXME Type safety issue when setting global socket-io object
   globalWithSocketIO.io = io
 
   io.on('connection', (socket: any) => {
@@ -44,7 +43,7 @@ const registerWebsocketEvents = (server: any) => {
     })
 
     socket.on('verifySvgInjectionChallenge', (data: any) => {
-      challengeUtils.solveIf(challenges.svgInjectionChallenge, () => { return data?.match(/.*\.\.\/\.\.\/\.\.[\w/-]*?\/redirect\?to=https?:\/\/placecats.com\/(g\/)?\d+/\) && security.isRedirectAllowed(data) })
+      challengeUtils.solveIf(challenges.svgInjectionChallenge, () => { return data?.match(/.*\.\.\/\.\.\/\.\.\/redirect\?to=https?:\/\/placecats.com\/(g\/)?[\d]+\/[\d]+.*/) && security.isRedirectAllowed(data) })
     })
 
     socket.on('verifyCloseNotificationsChallenge', (data: any) => {
